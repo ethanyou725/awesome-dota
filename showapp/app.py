b1482ace26690bc04ea40b0369d673ae539439ac
yzg <651992456@qq.com>
2016-11-28 15:35:26 +0800

    little

--- showapp/app.py
+++ showapp/app.py
@@ -1,6 +1,13 @@
-from flask import  Flask
 from flask_bootstrap import Bootstrap
+import sys
+from flask import Flask, render_template, flash, redirect, url_for, session
 
+
+# sys.path.append("..")
+# import scripts.DataHandler
+# import scripts.HeroList
+from scripts import DataHandler
+from scripts import HeroList
 
 
 app = Flask(__name__)
@@ -8,11 +15,19 @@ app.config['SECRET_KEY'] = 'today is 20161126'
 # testapp.config?????
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/error')
 def index():
-    return '<h1>404</h1>'
+    return '<h1>404 Not Found</h1>'
+
+@app.route('/')
+def home():
+    list=HeroList.Names
+    return render_template('show.html',post=list)
 
 
+@app.route('/hero/1')
+def detail():
+    pass
 
 
 if __name__ =="__main__":
